d06dd3832c2dc0e51aa7476b731d20fd00654080
James Exantus <81445832+JamesExantus@users.noreply.github.com>
2023-06-10 15:41:15 -0400

    Add files via upload

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,19 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let arr = {};
+    for (let i=0; i<word.length;i++) {
+        let character = word.charAt(i);
+        if (arr[character]) {
+           arr[character]++;
+        } else {
+           arr[character] = 1;
+        }
+    }
+
+    return arr;
+
 }
+
 
 // Do not edit this line;
 module.exports = frequencyCounter;
