32b9801994816ec78a8b74e5e3f34ecc0354e64d
James Exantus <81445832+JamesExantus@users.noreply.github.com>
2023-06-10 15:17:00 -0400

    Add files via upload

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,20 @@
 function isPalindrome(word) {
   // Insert code here;
+  
+  
+         
+        let palin = true;  
+        let l = word.length; 
+
+        for(let i = 0; i < l; i++)
+        {
+          if(word.charAt(i) != word.charAt(l-1-i)) {
+            
+            palin = false;
+         
+          }
+        }
+        return palin;
 }
 
 // Do not edit this line;
